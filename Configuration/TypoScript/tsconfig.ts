93885b58f64dfd61e3fdaef2ea8fdf38ae03a8f9
Philipp Zumstein <zuphilip@gmail.com>
2017-08-19 19:09:14 +0200

    New, improved frontend style APA
    This handles most of the points in
    https://github.com/UB-Mannheim/publist4ubma2/issues/20#issuecomment-321384418
    except the doi formating and indication of editors.

--- Configuration/TypoScript/tsconfig.ts
+++ Configuration/TypoScript/tsconfig.ts
@@ -3,4 +3,5 @@ tx_publist4ubma2.templateLayouts.1 = Debug Table
 tx_publist4ubma2.templateLayouts.2 = DWS - RDFA - Schema
 tx_publist4ubma2.templateLayouts.3 = BIB with Coins
 tx_publist4ubma2.templateLayouts.4 = BWL - APA
+tx_publist4ubma2.templateLayouts.5 = APA
 
