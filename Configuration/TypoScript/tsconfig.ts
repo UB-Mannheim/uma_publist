e5b8c0e504884c12c35017ce27a6110a76a81b9d
lsuma <32546522+lsuma@users.noreply.github.com>
2018-07-11 08:34:23 +0200

    Add template layout APA to tsconfig.ts

--- Configuration/TypoScript/tsconfig.ts
+++ Configuration/TypoScript/tsconfig.ts
@@ -3,4 +3,5 @@ tx_umapublist.templateLayouts.1 = Debug Table
 tx_umapublist.templateLayouts.2 = DWS - RDFA - Schema
 tx_umapublist.templateLayouts.3 = BIB with Coins
 tx_umapublist.templateLayouts.4 = BWL - APA
-tx_umapublist.templateLayouts.5 = Jura
+tx_umapublist.templateLayouts.5 = APA
+tx_umapublist.templateLayouts.6 = Jura
