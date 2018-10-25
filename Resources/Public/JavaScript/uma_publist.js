e1dec34a24b9a099601e0efb44eb40bca70ddf5d
Linus Stubert <stubert@rz.uni-mannheim.de>
2018-10-25 08:14:48 +0200

    Add TODO for filter function

--- Resources/Public/JavaScript/uma_publist.js
+++ Resources/Public/JavaScript/uma_publist.js
@@ -5,13 +5,17 @@ $.expr[":"].contains = $.expr.createPseudo(function(arg) {
 });
 
 $(document).ready(function() {
+	/*
+	TODO: add optional filter field to publication list template
+	*/
 	var updateUmaPublist = function(content, searchWord) {
 		searchWord = searchWord.replace(/'/g, '');
 		$(this).find('li').addClass('hidden').filter(":contains('" + searchWord + "')").removeClass('hidden');
 	};
 	$('.uma-publist-filter').on('keyup', function() {
-		updateFilter($('#' + $(this).attr('data-publist-content')), $(this).val());
+		updateUmaPublist($('#' + $(this).attr('data-publist-content')), $(this).val());
 	});
+
 	$('.uma-publist-hidden').hide();
 	$('.uma-publist-expand').on('click', function() {
 		var publist = $(this).closest('.uma-publist');
