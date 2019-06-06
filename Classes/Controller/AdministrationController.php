e7af95a4097ddec89b0ffb1f495121bc4d80a395
Linus Stubert <stubert@rz.uni-mannheim.de>
2019-06-06 15:55:56 +0200

    Add namespace in administration controller

--- Classes/Controller/AdministrationController.php
+++ Classes/Controller/AdministrationController.php
@@ -1,38 +1,11 @@
 <?php
 namespace UMA\UmaPublist\Controller;
 
-
-/***************************************************************
- *
- *  Copyright notice
- *
- *  (c) 2016 Sebastian Kotthoff <sebastian.kotthoff@rz.uni-mannheim.de>, Uni Mannheim
- *
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
-
-
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility as GeneralUtilityCore;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use UMA\UmaPublist\Utility\fileReader;
 use UMA\UmaPublist\Controller\InstituteController;
-
 
 /**
  * AdministrationController
